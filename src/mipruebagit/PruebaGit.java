fd3d78859ed4534c8e6e20b03e38fbd8884826bb
adrian9 <adri5485@gmail.com>
2013-04-09 22:04:03 +0300

    Update PruebaGit.java
    modificacion archivo java

--- src/mipruebagit/PruebaGit.java
+++ src/mipruebagit/PruebaGit.java
@@ -7,7 +7,6 @@ public class PruebaGit {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hola soy Adri�n y esto es Jackass");
-
 		System.out.println("Esto es una modificación para hacer un fetch");
 	}
 
