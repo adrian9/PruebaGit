ebb0dc83033eba92c8dc58f2eab434f5f2277a8a
roberto-asir <roberto.asir@gmail.com>
2013-04-15 14:15:43 +0300

    Update PruebaGit.java

--- src/mipruebagit/PruebaGit.java
+++ src/mipruebagit/PruebaGit.java
@@ -8,6 +8,7 @@ public class PruebaGit {
 	public static void main(String[] args) {
 		System.out.println("Hola soy Adrián y esto es Jackass");
 		System.out.println("Modificacion para fetch from");
+		System.out.println("Hola soy Roberto-asir y soy colaborador en el reposiorio de Adrian9");
 	}
 
 }
