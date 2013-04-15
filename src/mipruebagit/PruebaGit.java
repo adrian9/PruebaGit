14bbe368467c15fd078443c08b5a9168dfc0f734
adrian9 <adri5485@gmail.com>
2013-04-15 11:17:35 +0300

    Update PruebaGit.java

--- src/mipruebagit/PruebaGit.java
+++ src/mipruebagit/PruebaGit.java
@@ -7,8 +7,6 @@ public class PruebaGit {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hola soy Adriï¿½n y esto es Jackass");
-		System.out.println("Esto es una modificaciÃ³n 2 para hacer un fetch 3333333333333333");
-		system.out.println("Hola Adrián, que la fuerza te acompañe.")
 	}
 
 }
